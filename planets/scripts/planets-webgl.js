95a89c28513399db0eb4134887b65ac9f843a2e2
chipgw <gw.chip.gw@gmail.com>
2017-07-12 21:49:59 -0700

    Update Emscripten planets to 6b43b67.

--- planets/scripts/planets-webgl.js
+++ planets/scripts/planets-webgl.js
@@ -1,8 +1,13 @@
-var spheres, context, grid;
+var context, grid;
 
 var colorShader, colorCameraMat, colorModelMat, colorColor;
 var textureShader, textureCameraMat, textureViewMat, textureModelMat, textureLightDir;
 var planetTextureDiff, planetTextureNrm;
+var highResVBO, highResTriIBO, highResTriCount;
+var lowResVBO, lowResLineIBO, lowResLineCount;
+var circleVBO, circleLineIBO, circleLineCount;
+var arrowVBO, arrowIBO;
+var gridBuf;
 
 /* Convinience function to create a matrix with a position and scale value. */
 function makeMat(pos, scale) {
@@ -113,9 +118,71 @@ function initGL() {
     GLctx.uniform1i(GLctx.getUniformLocation(textureShader, "texture_diff"), 0);
     GLctx.uniform1i(GLctx.getUniformLocation(textureShader, "texture_nrm"), 1);
 
-    spheres = new Module.Spheres();
-
     grid = new Module.Grid();
+
+    gridBuf = GLctx.createBuffer();
+    GLctx.bindBuffer(GLctx.ARRAY_BUFFER, gridBuf);
+    GLctx.bufferData(GLctx.ARRAY_BUFFER, 0, GLctx.DYNAMIC_DRAW);
+
+    highResVBO = GLctx.createBuffer();
+    highResTriIBO = GLctx.createBuffer();
+    GLctx.bindBuffer(GLctx.ARRAY_BUFFER, highResVBO);
+    GLctx.bindBuffer(GLctx.ELEMENT_ARRAY_BUFFER, highResTriIBO);
+
+    highResTriCount = Module.genSolidSphere();
+
+    lowResVBO = GLctx.createBuffer();
+    lowResLineIBO = GLctx.createBuffer();
+    GLctx.bindBuffer(GLctx.ARRAY_BUFFER, lowResVBO);
+    GLctx.bindBuffer(GLctx.ELEMENT_ARRAY_BUFFER, lowResLineIBO);
+
+    lowResLineCount = Module.genWireSphere();
+
+    circleVBO = GLctx.createBuffer();
+    circleLineIBO = GLctx.createBuffer();
+    GLctx.bindBuffer(GLctx.ARRAY_BUFFER, circleVBO);
+    GLctx.bindBuffer(GLctx.ELEMENT_ARRAY_BUFFER, circleLineIBO);
+
+    circleLineCount = Module.genCircle();
+
+    arrowVBO = GLctx.createBuffer();
+    arrowIBO = GLctx.createBuffer();
+    GLctx.bindBuffer(GLctx.ARRAY_BUFFER, arrowVBO);
+    GLctx.bindBuffer(GLctx.ELEMENT_ARRAY_BUFFER, arrowIBO);
+
+    GLctx.bufferData(GLctx.ARRAY_BUFFER, new Float32Array([0.1, 0.1, 0.0,
+                                                           0.1,-0.1, 0.0,
+                                                          -0.1,-0.1, 0.0,
+                                                          -0.1, 0.1, 0.0,
+
+                                                           0.1, 0.1, 1.0,
+                                                           0.1,-0.1, 1.0,
+                                                          -0.1,-0.1, 1.0,
+                                                          -0.1, 0.1, 1.0,
+
+                                                           0.2, 0.2, 1.0,
+                                                           0.2,-0.2, 1.0,
+                                                          -0.2,-0.2, 1.0,
+                                                          -0.2, 0.2, 1.0,
+
+                                                           0.0, 0.0, 1.4 ]), GLctx.DYNAMIC_DRAW);
+
+    GLctx.bufferData(GLctx.ELEMENT_ARRAY_BUFFER, new Uint8Array([0,  1,  2,       2,  3,  0,
+
+                                                                 1,  0,  5,       4,  5,  0,
+                                                                 2,  1,  6,       5,  6,  1,
+                                                                 3,  2,  7,       6,  7,  2,
+                                                                 0,  3,  4,       7,  4,  3,
+
+                                                                 5,  4,  9,       8,  9,  4,
+                                                                 6,  5, 10,       9, 10,  5,
+                                                                 7,  6, 11,      10, 11,  6,
+                                                                 4,  7,  8,      11,  8,  7,
+
+                                                                 9,  8, 12,
+                                                                10,  9, 12,
+                                                                11, 10, 12,
+                                                                 8, 11, 12 ]), GLctx.STATIC_DRAW);
 
     return Promise.all([planetTextureDiffPromise, planetTextureNrmPromise]);
 }
@@ -160,7 +227,17 @@ function paint() {
     GLctx.activeTexture(GLctx.TEXTURE1);
     GLctx.bindTexture(GLctx.TEXTURE_2D, planetTextureNrm)
 
-    spheres.bindSolid()
+    GLctx.bindBuffer(GLctx.ARRAY_BUFFER, highResVBO);
+    GLctx.bindBuffer(GLctx.ELEMENT_ARRAY_BUFFER, highResTriIBO);
+
+    GLctx.enableVertexAttribArray(0);
+    GLctx.enableVertexAttribArray(1);
+    GLctx.enableVertexAttribArray(2);
+    GLctx.enableVertexAttribArray(3);
+    GLctx.vertexAttribPointer(0, 3, GLctx.FLOAT, GLctx.FALSE, 48, 0);
+    GLctx.vertexAttribPointer(1, 2, GLctx.FLOAT, GLctx.FALSE, 48, 36);
+    GLctx.vertexAttribPointer(2, 3, GLctx.FLOAT, GLctx.FALSE, 48, 12);
+    GLctx.vertexAttribPointer(3, 3, GLctx.FLOAT, GLctx.FALSE, 48, 24);
 
     /* Update the view-space light vector. */
     GLctx.uniform3fv(textureLightDir, camera.getLightDir());
@@ -168,33 +245,42 @@ function paint() {
     for (var i = 0; i < universe.size(); ++i) {
         GLctx.uniformMatrix4fv(textureModelMat, false, makeMat(universe.getPlanetPosition(i), universe.getPlanetRadius(i)));
 
-        spheres.drawSolid()
+        GLctx.drawElements(GLctx.TRIANGLES, highResTriCount, GLctx.UNSIGNED_INT, 0);
     }
+
+    GLctx.disableVertexAttribArray(1);
+    GLctx.disableVertexAttribArray(2);
+    GLctx.disableVertexAttribArray(3);
 
     GLctx.useProgram(colorShader);
 
     GLctx.uniformMatrix4fv(colorCameraMat, false, cameraMat);
 
-    spheres.bindWire()
+    GLctx.bindBuffer(GLctx.ARRAY_BUFFER, lowResVBO);
+    GLctx.bindBuffer(GLctx.ELEMENT_ARRAY_BUFFER, lowResLineIBO);
+    GLctx.vertexAttribPointer(0, 3, GLctx.FLOAT, GLctx.FALSE, 48, 0);
 
     GLctx.uniform4fv(colorColor, [0.0, 1.0, 0.0, 1.0]);
 
     if (placing.step !== Module.PlacingStep.NotPlacing && placing.step !== Module.PlacingStep.Firing) {
         GLctx.uniformMatrix4fv(colorModelMat, false, makeMat(placing.getPosition(),  placing.getRadius()));
 
-        spheres.drawWire()
+        GLctx.drawElements(GLctx.LINES, lowResLineCount, GLctx.UNSIGNED_INT, 0);
 
         GLctx.uniform4fv(colorColor, [1.0, 1.0, 1.0, 1.0]);
 
         /* Draw two circles to show the relative orbits of both the planets. */
         if ((placing.step === Module.PlacingStep.OrbitalPlane || placing.step === Module.PlacingStep.OrbitalPlanet) && universe.isSelectedValid()) {
-            spheres.bindCircle();
+            GLctx.bindBuffer(GLctx.ARRAY_BUFFER, circleVBO);
+            GLctx.bindBuffer(GLctx.ELEMENT_ARRAY_BUFFER, circleLineIBO);
+
+            GLctx.vertexAttribPointer(0, 3, GLctx.FLOAT, 0, 12, 0);
 
             GLctx.uniformMatrix4fv(colorModelMat, false, placing.getOrbitalCircleMat());
-            spheres.drawCircle();
+            GLctx.drawElements(GLctx.LINES, circleLineCount, GLctx.UNSIGNED_INT, 0);
 
             GLctx.uniformMatrix4fv(colorModelMat, false, placing.getOrbitedCircleMat());
-            spheres.drawCircle();
+            GLctx.drawElements(GLctx.LINES, circleLineCount, GLctx.UNSIGNED_INT, 0);
         }
 
         if (placing.step === Module.PlacingStep.FreeVelocity) {
@@ -202,13 +288,20 @@ function paint() {
 
             GLctx.uniformMatrix4fv(colorModelMat, false, placing.getArrowMat());
 
-            spheres.drawArrow(length);
+            GLctx.bindBuffer(GLctx.ARRAY_BUFFER, arrowVBO);
+            GLctx.bindBuffer(GLctx.ELEMENT_ARRAY_BUFFER, arrowIBO);
+
+            /* Keep the tip of the arrow from stretching. */
+            GLctx.bufferSubData(GLctx.ARRAY_BUFFER, 152, new Float32Array([ 1.0 + 0.4 / length]));
+
+            GLctx.vertexAttribPointer(0, 3, GLctx.FLOAT, 0, 0, 0);
+            GLctx.drawElements(GLctx.TRIANGLES, 66, GLctx.UNSIGNED_BYTE, 0);
         }
     } else if (universe.isSelectedValid()) {
         GLctx.uniformMatrix4fv(colorModelMat, false, makeMat(universe.getPlanetPosition(universe.selected),
                                                              universe.getPlanetRadius(universe.selected) * 1.02));
 
-        spheres.drawWire();
+        GLctx.drawElements(GLctx.LINES, lowResLineCount, GLctx.UNSIGNED_INT, 0);
     }
 
     /* A circle in the center of the view to show where the gamepad is focused.
@@ -220,14 +313,15 @@ function paint() {
 
         GLctx.uniformMatrix4fv(colorModelMat, false, makeMat(camera.position, camera.distance * 4.0e-3));
 
-        spheres.drawCircle();
+        GLctx.drawElements(GLctx.LINES, circleLineCount, GLctx.UNSIGNED_INT, 0);
         GLctx.enable(GLctx.DEPTH_TEST);
     }
 
     if (grid.enabled) {
+        GLctx.bindBuffer(GLctx.ARRAY_BUFFER, gridBuf);
         grid.update(camera);
 
-        grid.bind();
+        GLctx.vertexAttribPointer(0, 2, GLctx.FLOAT, 0, 0, 0);
 
         /* Don't write to depth buffer. */
         GLctx.depthMask(false);
@@ -242,7 +336,7 @@ function paint() {
         GLctx.uniform4fv(colorColor, color);
 
         /* Draw the large grid. */
-        grid.draw();
+        GLctx.drawArrays(GLctx.LINES, 0, grid.numPoints());
 
         /* Modify the matrix to be half the size. */
         gridMat[0] *= 0.5; gridMat[5] *= 0.5; gridMat[10] *= 0.5;
@@ -252,7 +346,7 @@ function paint() {
         GLctx.uniform4fv(colorColor, color);
 
         /* Draw the small grid. */
-        grid.draw();
+        GLctx.drawArrays(GLctx.LINES, 0, grid.numPoints());
 
         GLctx.depthMask(true);
     }
